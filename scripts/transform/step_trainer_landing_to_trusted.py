128dd3e1b18d2764664c0d87a2d0cbe62f1f1731
Khuong Nguyen Ba <nguyenbakhuong64@gmail.com>
2024-12-14 20:00:01 +0100

    Modify step_trainer landing to trusted

--- scripts/transform/step_trainer_landing_to_trusted.py
+++ scripts/transform/step_trainer_landing_to_trusted.py
@@ -4,6 +4,13 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue import DynamicFrame
+
+def spark_sql_query(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -20,7 +27,7 @@ customer_curated = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
     format="json",
     connection_options={
-        "paths": ["s3://human-balance/customer/curated"],
+        "paths": ["s3://mys3nbk/customer/curated"],
         "recurse": True,
     },
     transformation_ctx="customer_curated",
@@ -32,18 +39,25 @@ step_trainer_landing = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
     format="json",
     connection_options={
-        "paths": ["s3://human-balance/step_trainer/landing"],
+        "paths": ["s3://mys3nbk/step_trainer/landing"],
         "recurse": True,
     },
     transformation_ctx="step_trainer_landing",
 ) 
 
 # Join data from customer_curated with step_trainer_landing
-joined_data = Join.apply(
-    frame1=customer_curated,
-    frame2=step_trainer_landing,
-    keys1=["serialnumber"],
-    keys2=["serialnumber"],
+sql_query = """
+select DISTINCT step_trainer_landing.* 
+from step_trainer_landing
+INNER JOIN customer_curated on customer_curated.serialnumber=step_trainer_landing.serialnumber;
+"""
+joined_data = spark_sql_query(
+    glueContext,
+    query=sql_query,
+    mapping={
+        "customer_curated": customer_curated,
+        "step_trainer_landing": step_trainer_landing,
+    },
     transformation_ctx="joined_data",
 )
 
@@ -53,7 +67,7 @@ step_trainer_trusted = glueContext.write_dynamic_frame.from_options(
     connection_type="s3",
     format="json",
     connection_options={
-        "path": "s3://human-balance/step_trainer/trusted",
+        "path": "s3://mys3nbk/step_trainer/trusted",
         "partitionKeys": [],
     },
     transformation_ctx="step_trainer_trusted",
